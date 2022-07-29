2fdbf6a097e4898e87f035b04d8a56abd63c895d
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2022-07-29 16:09:20 +0200

    Add and Update tested for ingredients

--- SQLScripts/data.sql
+++ SQLScripts/data.sql
@@ -1,4 +1,4 @@
-# Ingredients
+SET FOREIGN_KEY_CHECKS = 0;
 insert into Ingredient (id, name, type)
 values ('FLTO', 'Flour Tortilla', 'WRAP');
 insert into Ingredient (id, name, type)
@@ -20,8 +20,6 @@ values ('SLSA', 'Salsa', 'SAUCE');
 insert into Ingredient (id, name, type)
 values ('SRCR', 'Sour Cream', 'SAUCE');
 
-# Taco Order
-
 insert into Taco_Order (id, delivery_Name, delivery_Street, delivery_City, delivery_State, delivery_Zip, cc_number,
                         cc_expiration, cc_cvv, email)
 VALUES (1, 'My first order', 'Kloosterstraat 27 a', 'Emblem', 'Antwerpen', '2520', '4111111111111112', '10/12', '999', 'test@email.com');
@@ -30,7 +28,6 @@ insert into Taco_Order (id, delivery_Name, delivery_Street, delivery_City, deliv
                         cc_expiration, cc_cvv, email)
 VALUES (2, 'Test McTest', '1234 Test Lane', 'Testville', 'CO', '80123', '4111111111111111', '8/12', '124', 'test2@email.com');
 
-# Tacos
 insert into Taco(id, name, taco_order_id)
 values (1, 'Taco one', 1);
 
@@ -39,8 +36,6 @@ values (2, 'Taco two', 1);
 
 insert into Taco(id, name, taco_order_id)
 values (3, 'Taco three', 2);
-
-# taco_to_ingredient
 
 insert into Taco_to_Ingredient (taco_id, ingredient_id)
 values (1, 'FLTO');
@@ -58,3 +53,4 @@ insert into Taco_to_Ingredient (taco_id, ingredient_id)
 values (3, 'CARN');
 insert into Taco_to_Ingredient (taco_id, ingredient_id)
 values (3, 'FLTO');
+SET FOREIGN_KEY_CHECKS = 1;
