90ec45ca872cdd3526f5c13dca0bdb4f5bf9d4b4
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2022-10-05 12:55:36 +0300

    minor cleanup

--- src/main/java/com/falcontech/tacoe001cud/model/Orderer.java
+++ src/main/java/com/falcontech/tacoe001cud/model/Orderer.java
@@ -20,7 +20,6 @@ import java.util.UUID;
 @Table(name = "Orderer")
 public class Orderer {
   @Id
-//  @GeneratedValue(generator = "uuid2")
   @GenericGenerator(name = "uuid2", strategy = "uuid2")
   @Column(columnDefinition = "BINARY(16)")
   private UUID id;
